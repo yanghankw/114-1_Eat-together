9b67b66642ffb7a4c4a6c4390736609985bbe5d9
Henry <henryyuen200241@gmail.com>
2025-12-20 17:07:39 +0800

    2025/12/20  5 commit: 修改了後端註冊登入的功能，現在能夠正常的連結到supabase完成註冊與登入。還有檔案位置的修改，請留意一下。要測試的話先開啟手機模擬器，再Server開啟

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:21.0.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("com.google.android.libraries.places:places:3.3.0")
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
